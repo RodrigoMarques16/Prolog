8b03cc820e30c90d18e4897043d98ce487d099ae
Rodrigo Marques <rodrigocanedomarques@hotmail.com>
2018-11-21 12:31:40 +0000

    fixes to simplification

--- Assignment 1/mono.pl
+++ Assignment 1/mono.pl
@@ -9,10 +9,8 @@
 %   - Coefficient * Var power: K * x^Exp
 %   - Var Power * Coefficient: x^Exp * K
 %
-monomial(X*K) :- number(K), power(X).
 monomial(K*X) :- number(K), power(X).
-%monomial(V)   :- pvar(V).
-%monomial(-V)  :- pvar(V).
+monomial(X*K) :- number(K), power(X).
 monomial(VP)  :- power(VP).
 monomial(-VP) :- power(VP).
 monomial(N)   :- number(N).
@@ -49,19 +47,22 @@ normalize_mono(M, M2) :-
 % If both monomials have the same var power the sum
 % results in a polynomial.
 %
-addmono(M1, M2, M4) :-
+addmono(M, 0, M) :- !.
+addmono(0, M, M) :- !.
+
+addmono(M1, M2, M) :-
     monparts(M1, K1, VP1),
     monparts(M2, K2, VP1),
     K3 is K1+K2,
     M3 = K3 * VP1, 
-    simmono(M3, M4),
+    simmono(M3, M),
     !.
 
-addmono(M1, M2, M3) :-
+addmono(M1, M2, M) :-
     monparts(M1, _, VP1),
     monparts(M2, _, VP2),
     VP1 \== VP2,
-    M3 = M1 + M2, 
+    M = M1 + M2, 
     !.
 
 %% scalemono(M1, K, M2)
@@ -84,14 +85,14 @@ simmono(M1, M) :-
     normalize_mono(M1, M2),
     simmono_aux(M2, M).
 
+simmono_aux(indep, 1)     :- !.
+simmono_aux(indep^1, 1)   :- !.
+simmono_aux(K*indep, K)   :- number(K),           !.
+simmono_aux(K*indep^1, K) :- number(K),           !.
 simmono_aux(-1*_^0, -1)   :- !.
 simmono_aux(1*VP, M2)     :- simmono_aux(VP, M2), !.
 simmono_aux(-1*VP, -M2)   :- simmono_aux(VP, M2), !.
 simmono_aux(0*_, 0)       :- !.
-simmono_aux(K*indep, K)   :- number(K),           !.
-simmono_aux(K*indep^1, K) :- number(K),           !.
-simmono_aux(indep, 1)     :- !.
-simmono_aux(indep^1, 1)   :- !.
 simmono_aux(K*V^1, K*V)   :- coefficient(K),      !.
 simmono_aux(V^1, V)       :- pvar(V),             !. 
 simmono_aux(-V^1, -V)     :- pvar(V),             !. 
